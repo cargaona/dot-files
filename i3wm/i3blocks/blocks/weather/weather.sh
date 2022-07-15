e454e4950c8da7ca47746a4bf006b08404f85aac
charlie <carlosmartingaona@gmail.com>
2022-07-15 15:04:33 -0300

    .

--- i3wm/i3blocks/blocks/weather/weather.sh
+++ i3wm/i3blocks/blocks/weather/weather.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cosa=$(curl https://wttr.in/BuenosAires\?format\=j1)
-description= echo $cosa | jq -r '."current_condition"[0]."weatherDesc"[0]."value"'
-temperature= echo $cosa | jq -r '."current_condition"[0]."temp_C"'
-feelslike= echo $cosa |  jq -r '."current_condition"[0]."FeelsLikeC"'
-msg= ${description}  $temperature  $feelslike
-echo $msg
+#description= echo $cosa | jq -r '."current_condition"[0]."weatherDesc"[0]."value"'
+#temperature= echo $cosa | jq -r '."current_condition"[0]."temp_C"'
+#feelslike= echo $cosa |  jq -r '."current_condition"[0]."FeelsLikeC"'
+#msg= ${description}  $temperature  $feelslike
+echo $cosa | jq . | notify-send 
+#notify-send $msg
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -84,7 +84,7 @@ if ! kill -0 $SSH_AGENT_PID 2>/dev/null; then
         eval `cat ~/.ssh.agent` >/dev/null
 fi
 
-bindkey -M menuselect '^[[Z' reverse-menu-complete
+#bindkey -M menuselect '^[[Z' reverse-menu-complete
 
 if [[ "${terminfo[kcuu1]}" != "" ]]; then
     autoload -U up-line-or-beginning-search
@@ -147,6 +147,7 @@ alias bt=bluetoothctl
 alias btoff="bluetoothctl power off"
 alias bton="bluetoothctl power on"
 
+alias ac="aws sts get-caller-identity"
 alias kctx=kubectx
 alias k=kubectl
 alias kon=kubeon
