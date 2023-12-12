7014bc6bf7ad326d95e1a784534305a8f7cba1a3
charlie <charlie@charlei.xyz>
2023-12-12 11:11:29 -0300

    updated in mac

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -204,11 +204,6 @@ _G.packer_plugins = {
     path = "/Users/cgaona/.local/share/nvim/site/pack/packer/start/vim-go",
     url = "https://github.com/fatih/vim-go"
   },
-  ["vim-jsx-pretty"] = {
-    loaded = true,
-    path = "/Users/cgaona/.local/share/nvim/site/pack/packer/start/vim-jsx-pretty",
-    url = "https://github.com/maxmellon/vim-jsx-pretty"
-  },
   ["vim-markdown"] = {
     loaded = true,
     path = "/Users/cgaona/.local/share/nvim/site/pack/packer/start/vim-markdown",
