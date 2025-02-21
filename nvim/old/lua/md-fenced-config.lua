399d1fdc44fcee0ee10105f43c1538179b67b495
charlie <charlie@charlei.xyz>
2025-02-20 21:47:01 -0300

    deleted old nvim folder

--- nvim/old/lua/md-fenced-config.lua
+++
@@ -1,6 +0,0 @@
--- Markdown code highlight
-vim.g['markdown_fenced_languages'] = {
-    'julia', 'css', 'python', 'javascript', 'js=javascript', 'json=javascript',
-    'ruby', 'sass', 'html', 'go','typescript'
-}
-
--- nvim/old/lua/schlepp-config.lua
+++
@@ -1,3 +0,0 @@
-vim.g['Schlepp#allowSquishingLines'] = 1
-vim.g['Schlepp#allowSquishingBlock'] = 1
-vim.g['Schlepp#trimWS'] = 0
