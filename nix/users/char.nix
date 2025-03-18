7e246b6971c9c08d9c1de5805a2e97e98d1c8d48
charlie <charlie@charlie.com>
2025-03-18 17:06:49 -0300

    feat: ssh - add public key to authorized keys

--- nix/users/char.nix
+++ nix/users/char.nix
@@ -9,12 +9,14 @@ in
 {
   # --- User Definitions ---
   users.users.char = {
+    openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCiytR0RDJVFPwlPA+yBKUc6KhukzlPHCZ3ckaEZz5f5cXHp3g5dKZyOd7Kju7fEWa0apb37W9QZPa1PvreeJu6wpBs7FSNC6NpyjwnRjfpfbU1XC8WEVLzzgke+dV/7Ap98K2qpUoYAFIbYlAdGJAzMOc9BYKrb6l2ARIA26Fc2Zd7fUkdr8jcMQFv5qnFju1e82iPggeymNHKd7RNkuAg4nAfkTwNKxzcoF3rTBCc0iG0TE61uUbVOx6CVS/17Xj7g+YKS2jMuDE3nWi3Fl1PUx5J07DVFF4TzyxM2Srn8V39QSoCQDevyN1PZJcRW34GtgG2y1EEfJNVR1sP+GHak037L78JSyLfaDTB3uJzICOTdifFrc88SuauU+KMyyMnrP3h2YuWWurtOAZMGpPZlocFt8ILpC9j43fRtNbLwVe3dPTJDlDJJTZMjEVNrT1S6Xi6r7bYpq5jZgWNvPqByHnySrMsSwIGbuFfPoawVzbye6uliK8REiQV1azlMn0Lmv+YRwTURnpyiDmyAR3OxrB0CN9a0B8MLzp+IY1ZrLBxMCC/yRIWih2dOiLTri45qrt4Sul2NZYYPM9I2iwA8xezvp/Dr27gUKkixQVsCprRT8+J6fYVcVDOnzkDej/Hyem6oJqsGeSkjCmGCebBqJvxd5ZODD74A5HZzkLcrQ== cardno:15_206_941"];
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user
-    packages = with pkgs; [
+      packages = with pkgs; [
       tree
     ];
   };
+
 
   # Default user shell (Zsh)
   users.defaultUserShell = pkgs.zsh;
