1400ff6b1b623220ce0e96202aaff0a7d8b038d8
nq@nq.io <nq@nq.io>
2025-07-04 19:52:24 -0300

    feat: add cosmic

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,9 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
   };
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, nixos-cosmic, ... }:
     {
       nixosConfigurations = {
         "desktop" = nixpkgs.lib.nixosSystem {
@@ -14,6 +15,18 @@
         "server" = nixpkgs.lib.nixosSystem {
           modules = [
             /etc/nixos/hosts/server/configuration.nix
+          ];
+        };
+        "macbook" = nixpkgs.lib.nixosSystem {
+          modules = [
+            {
+              nix.settings = {
+                substituters = [ "https://cosmic.cachix.org/" ];
+                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+              };
+            }
+            nixos-cosmic.nixosModules.default
+            /etc/nixos/hosts/notebook/configuration.nix
           ];
         };
       };
--- nix/hosts/notebook/configuration.nix
+++ nix/hosts/notebook/configuration.nix
@@ -6,22 +6,26 @@
   imports = [
     # Include home-manager configuration and other modular settings
     <home-manager/nixos>
-    ./packages.nix
-    ./services/audio.nix
-    ./services/docker.nix
-    ./services/ssh.nix
-    ./services/power.nix
-    ./services/sync.nix
-    ./sys/fonts.nix
-    ./sys/graphics.nix
-    ./sys/input.nix
-    ./services/server.nix
-    ./services/wireguard.nix
-    ./users/char.nix
+    ../../packages.nix
+    ../../services/audio.nix
+    # ../../services/docker.nix
+    ../../services/ssh.nix
+    ../../sys/hyprland.nix
+    ../../services/power.nix
+    ../../services/sync.nix
+    ../../sys/fonts.nix
+    # ../../sys/graphics.nix
+    ../../sys/input.nix
+    ../../services/server.nix
+    #    ../../services/wireguard.nix
+    ../../users/char.nix
     /etc/nixos/hardware-configuration.nix
   ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes"];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   # Bootloader Configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -29,19 +33,19 @@
   # Networking Configuration
   networking.hostName = "mbp"; # Define your hostname
   networking.networkmanager.enable = true;
-  networking.firewall.allowedTCPPorts = [
-    7070
-    8096
-    8080
-    4443
-    8112
-    11434 ## ollama
-    4040 ## openwebui
-    9998 ## tika
-  ];
-  networking.firewall = {
-    allowedUDPPorts = [ 51820 ];
-  };
+  # networking.firewall.allowedTCPPorts = [
+  #   7070
+  #   8096
+  #   8080
+  #   4443
+  #   8112
+  #   11434 ## ollama
+  #   4040 ## openwebui
+  #   9998 ## tika
+  # ];
+  # networking.firewall = {
+  #   allowedUDPPorts = [ 51820 ];
+  # };
 
   # Time Zone Configuration
   time.timeZone = "America/Argentina/Buenos_Aires";
@@ -55,6 +59,8 @@
   # Allow Unfree Packages (like Spotify, Steam)
   nixpkgs.config.allowUnfree = true;
 
+  services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
   #https://askubuntu.com/questions/1434722/macbook-takes-20-seconds-to-wake-up
   # only for macbook
   boot.kernelParams = [ "mem_sleep_default=s2idle" ];
